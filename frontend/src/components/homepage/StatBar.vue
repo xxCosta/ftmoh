51f6c01cd3c970573e4f5634c1d081476c1371ba
saucedbenny <langley.ben98@gmail.com>
2024-06-15 23:43:03 +0000

    init frontend
    got rid of everything a dont need and added some base utility

---
+++ frontend/src/components/homepage/StatBar.vue
@@ -0,0 +1,21 @@
+<script setup lang="ts">
+</script>
+
+<template>
+    <div class="section">
+    <h2 class="oxygen-light">Balance: </h2>
+    <h2 class="oxygen-light">RR(avg):  </h2>
+    <h2 class="oxygen-light">Win Rate: </h2>
+    <h2 class="oxygen-light">Avg. Profit: </h2>
+    <h2 class="oxygen-light">Avg. Lostt: </h2>
+    </div>
+</template>
+
+<style>
+.section{
+    width: 100vw;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-evenly
+}
+</style>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,7 +1,9 @@
 <script setup lang="ts">
+import StatBar from '@/components/homepage/StatBar.vue'
 </script>
 
 <template>
   <main>
+      <StatBar />
   </main>
 </template>
