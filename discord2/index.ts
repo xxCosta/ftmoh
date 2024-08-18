ddb06f999bbe51c7dadd188c34702c64324bd90e
ben <langley.ben98@gmail.com>
2024-08-18 02:23:38 +0000

    feat: including types file

--- discord2/index.ts
+++ discord2/index.ts
@@ -1,5 +1,5 @@
 import {Elysia, t} from 'elysia'
-
+import {} from './types'
 
 //discord websocker url
 let dws:string
---
+++ discord2/types.ts
@@ -0,0 +1,45 @@
+
+
+export interface SendEvent{
+    op: number,
+    d: object
+}
+
+export interface TradeProps {
+    symbol: string,
+    profit: number,
+    endTime: string,
+    type: number,
+    positionSize: number,
+    tp: number,
+    sl: number,
+    accountBalance: number
+}
+
+
+export class Trade implements TradeProps {
+    symbol
+    profit
+    endTime
+    type
+    positionSize
+    tp
+    sl
+    accountBalance
+
+    constructor(symbol: string, profit: number, endTime: string, type: number, positionSize: number, tp: number, sl: number, accountBalance: number) {
+        this.symbol = symbol,
+            this.profit = profit,
+            this.endTime = endTime,
+            this.type = type,
+            this.positionSize = positionSize,
+            this.tp = tp,
+            this.sl = sl,
+            this.accountBalance = accountBalance
+    }
+}
+
+
+
+
+
