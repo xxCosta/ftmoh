32e30ba3abdb8a979c8dac936338bb75f571be83
ben <langley.ben98@gmail.com>
2024-08-18 01:49:17 +0000

    ill explain when youre older

---
+++ discord2/index.ts
@@ -0,0 +1,36 @@
+import {Elysia, t} from 'elysia'
+
+
+//discord websocker url
+let dws:string
+
+try{
+
+    const headerInfo = {
+        "Authorization": `Bot ${Bun.env.TOKEN}`
+    }
+
+    const headers = new Headers(headerInfo)
+
+    const response = await fetch("https://discord.com/api/gateway/bot", {
+        headers: headers
+    })
+    const result = await response.json()
+    dws = result.url 
+
+}catch (e){
+    console.log(e)
+}
+
+const server = new Elysia()
+    .get('/', () => 'was poppin')
+    .listen(3000)
+
+console.log(`server ready on port ${server.server!.url.port}`)
+
+
+
+
+
+
+
